e8bfb007e8ad96bbecc906f82e19c2df1c936555
Aaron Slaughter <aslaugh1@gmail.com>
2022-02-03 12:28:47 -0500

    added skeleton files for all components

---
+++ wish_gift_client/src/components/About.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <div>About Placeholder</div>
+    <p>TODO: include an explanation of how that app works</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+}
+</script>
---
+++ wish_gift_client/src/components/NewList.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <div>New List Placeholder</div>
+    <p>TODO: make a form to create a new list</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NewList',
+}
+</script>
