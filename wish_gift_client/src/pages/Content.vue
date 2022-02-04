afd97923c789dacc2b1d1dbda81b2629f0e99f35
Aaron Slaughter <aslaugh1@gmail.com>
2022-02-03 20:14:12 -0500

    added submit-able form to newList component

--- wish_gift_client/src/pages/Content.vue
+++ wish_gift_client/src/pages/Content.vue
@@ -2,9 +2,19 @@
   <div>
     <div>Content Placeholder</div>
     <About/>
-    <NewList/>
-    <CreatedList/>
+    <div v-if="!previousList && !createdList">
+      <NewList
+        :name='name'
+        :listItem='listItem'
+        @handleFormChange="handleFormChange"
+        @handleSubmit="handleSubmit"
+      />
+    </div>
+    <div v-else>
+      <CreatedList/>
     <PreviousList/>
+    </div>
+    
   </div>
 </template>
 
@@ -21,6 +31,27 @@ export default {
     CreatedList,
     NewList,
     PreviousList,
+  },
+  data: () => ({
+    previousList: null,
+    createdList: null,
+    name: '',
+    listItem: ''
+  }),
+  methods: {
+    handleFormChange(name, value) {
+      this[name] = value
+    },
+    handleSubmit() {
+      // TODO
+      // api call here to assign previousList and createdList
+      alert('Form Submitted')
+
+      // TODO
+      this.previousList = {placeholder: 'placeholder'}
+      this.createdList = {placeholder: 'placeholder'}
+
+    }
   }
 }
 </script>
