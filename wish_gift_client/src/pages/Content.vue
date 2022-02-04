0acbbd27bd0e5e4eae406f58f69eb6f45a30724f
julio <julior0518@gmail.com>
2022-02-04 12:27:01 -0500

    initial style page done

--- wish_gift_client/src/pages/Content.vue
+++ wish_gift_client/src/pages/Content.vue
@@ -1,20 +1,21 @@
 <template>
-  <div>
-    <div>Content Placeholder</div>
-    <About/>
-    <div v-if="!previousList && !createdList">
-      <NewList
-        :name='name'
-        :listItem='listItem'
-        @handleFormChange="handleFormChange"
-        @handleSubmit="handleSubmit"
-      />
+  <div id="content">
+    <div class="margin">
+        <h2 >Content Placeholder</h2>
+        <About/>
+        <div v-if="!previousList && !createdList">
+          <NewList
+            :name='name'
+            :listItem='listItem'
+            @handleFormChange="handleFormChange"
+            @handleSubmit="handleSubmit"
+          />
+        </div>
+        <div v-else>
+          <CreatedList/>
+          <PreviousList/>
+        </div>
     </div>
-    <div v-else>
-      <CreatedList/>
-    <PreviousList/>
-    </div>
-    
   </div>
 </template>
 
@@ -55,3 +56,33 @@ export default {
   }
 }
 </script>
+
+
+<style>
+#content {
+  border: 1px solid rgb(126, 126, 126);
+  position: absolute;
+  left:24vw;
+  top:10%;
+  width:50vw;
+  background-color: whitesmoke;
+  box-shadow: 20px 20px 35px rgba(0, 0, 0, 0.671);
+}
+#content:hover {
+  box-shadow: 10px 10px 25px rgba(0, 0, 0, 0.671);
+  color: black
+  ;
+}
+.margin{
+  border-left: 2px solid rgba(194, 47, 47, 0.39);
+  padding-top:60px;
+  height: 100vh;
+  text-align: left;
+  margin: 0 80px;
+  text-indent: 20px;
+
+}
+
+
+
+</style>
