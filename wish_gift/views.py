735cb9394626738c9ab0b56d2b76e38240e8184a
janemzwangnj <janemzwangnj@gmail.com>
2022-02-03 22:38:48 -0500

    get, getbypk, and delete on List are done

--- wish_gift/views.py
+++ wish_gift/views.py
@@ -1,6 +1,35 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+# views.py
+from django.http import JsonResponse
+from .models import List
+from django.shortcuts import redirect
+from .forms import ListForm
 
-from django.shortcuts import render
 
-# Create your views here.
+def gift_list(request):
+    gifts = List.objects.all().values('owner', 'listItem')
+    gift_list = list(gifts)
+    return JsonResponse(gift_list, safe=False)
+
+
+def gift_detail(request, pk):
+    gift = List.objects.get(id=pk)
+    data = {'owner': gift.owner, 'listItem': gift.listItem}
+    return JsonResponse(data, safe=False)
+
+
+# def gift_create(request):
+#     if request.method == 'POST':
+#         form = ListForm(request.POST)
+#         if form.is_valid():
+#             list = form.save()
+#             return redirect('list_detail', pk=list.pk)
+#     else:
+#         form = ListForm()
+#     # return JsonResponse(request, 'tunr/artist_form.html', {'form': form})
+#     # data = {'owner': form.owner, 'listItem': form.listItem}
+#     # return JsonResponse(data, safe=False)
+
+
+def gift_delete(request, pk):
+    List.objects.get(id=pk).delete()
+    return redirect('gift_list')
--- wish_gift_django/settings.py
+++ wish_gift_django/settings.py
@@ -79,8 +79,11 @@ WSGI_APPLICATION = 'wish_gift_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'wish_gift',
+        'USER': 'wish_gift_user',
+        'PASSWORD': 'wish_gift',
+        'HOST': 'localhost'
     }
 }
 
