802453600b30fa85092e3f1e1e12664cce6d1e0d
Terra Smith <t_m@linuxmail.org>
2022-02-04 10:56:24 -0800

    modified the about.vue file

--- wish_gift_client/src/components/About.vue
+++ wish_gift_client/src/components/About.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="about">
-    <p>Wish_Gift is a fast growing app that allows friends and family to send and receive gifts, Secret Santa style...</p>
-    <p>Friends, family and co-workers can add the number of people in the group and share their lists among each other. Once the group has entered in their wishes, they will have one chance to make any changes, after which, users will not be able to edit and the wishlist will become permanent.</p>
-    <p>Using Wish_Gift is an easy way to let your friends and family know your favorite products and help them buy you something you actually like</p>
+    <p>Wish Gift is a fast growing app that allows friends, family and co-workers to send and receive gifts from each other, Secret Santa style...</p>
+    
+    <p>Using Wish Gift is an easy way to let your friends and family know your favorite products and help them buy you something you actually like</p>
   </div>
 </template>
 
