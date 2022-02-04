c128bcf7d5f2e86590ab42447d25d560adeb4d1f
janemzwangnj <janemzwangnj@gmail.com>
2022-02-04 02:41:01 -0500

    post function with bug

--- wish_gift/views.py
+++ wish_gift/views.py
@@ -6,28 +6,24 @@ from .forms import ListForm
 
 
 def gift_list(request):
-    gifts = List.objects.all().values('owner', 'listItem')
+    gifts = List.objects.all().values('id', 'owner', 'listItem')
     gift_list = list(gifts)
     return JsonResponse(gift_list, safe=False)
 
 
 def gift_detail(request, pk):
     gift = List.objects.get(id=pk)
-    data = {'owner': gift.owner, 'listItem': gift.listItem}
+    data = {'id': gift.id, 'owner': gift.owner, 'listItem': gift.listItem}
     return JsonResponse(data, safe=False)
 
 
-# def gift_create(request):
-#     if request.method == 'POST':
-#         form = ListForm(request.POST)
-#         if form.is_valid():
-#             list = form.save()
-#             return redirect('list_detail', pk=list.pk)
-#     else:
-#         form = ListForm()
-#     # return JsonResponse(request, 'tunr/artist_form.html', {'form': form})
-#     # data = {'owner': form.owner, 'listItem': form.listItem}
-#     # return JsonResponse(data, safe=False)
+# def gift_create(request, owner1, listItem1):
+def gift_create(request):
+    print(request)
+    # owner = request.data.get('owner')
+    # listItem = request.data.get('listItem')
+    gift = List.objects.create(owner="Jashi", listItem="Computer")
+    return redirect('gift_list')
 
 
 def gift_delete(request, pk):
